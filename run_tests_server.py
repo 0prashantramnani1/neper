38522eb2c00364c36f9ed9c84d658d3b067344ba
ramnani <ramnani@localhost>
2023-01-31 22:48:20 -0700

    Test script update

--- run_tests_server.py
+++ run_tests_server.py
@@ -12,19 +12,18 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv_addr = ("0.0.0.0", control_plane_port)
 sock.bind(serv_addr)
 
-sock.listen()
+#sock.listen()
 #conn, addr = sock.accept()
 
 #./tests/netperf_server server.config 1 10
-command      = "./tcp_rr"
+command      = "./tcp_stream"
 
 payload_size = 16384
 
-while payload_size < 32000:
-    flows = 10    
-    while flows < 150:
-        process = subprocess.run([command, "-F", str(flows), "-B", str(payload_size)])
-        flows += 20
-    payload_size *= 2
+flows = 10    
+
+while flows < 32000:
+    process = subprocess.run([command, "-F", str(flows), "-B", str(payload_size)])
+    flows += 1000
 
 sock.close()
