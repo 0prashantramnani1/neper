61c2bdd379a096cf2b42bc1d094bff1351c6d556
ramnani <ramnani@localhost>
2023-01-31 22:49:46 -0700

    Test scripts update -client

--- run_tests_client.py
+++ run_tests_client.py
@@ -12,33 +12,31 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv_addr = ("0.0.0.0", control_plane_port)
 sock.bind(serv_addr)
 
-sock.listen()
+#sock.listen()
 #conn, addr = sock.accept()
 
 #./tests/netperf_server server.config 1 10
-command      = "./tcp_rr"
+command      = "./tcp_stream"
 encoding = 'utf-8'
 payload_size = 16384
 
-file1 = open("test_neper_16000.txt", "w")
+file1 = open("test_neper_100g.txt", "w")
 
-while payload_size < 32000:
-    file1.write("payload_size: {}\n".format(payload_size))
-    flows = 10
-    while flows < 151:
-        process = subprocess.check_output([command, "-c", "-H", "128.110.218.45", "-F", str(flows), "-B", str(payload_size)])
-        output = process.decode(encoding)
-        for line in output.splitlines():
-                if "throughput" in line:
-                    a = line.split("=")[1]
-                    a = float(a)
+flows = 10
+
+while flows < 3200:
+    process = subprocess.check_output([command, "-c", "-H", "10.10.1.1", "-F", str(flows), "-T", str(1)])
+    output = process.decode(encoding)
+    for line in output.splitlines():
+            if "remote_throughput" in line:
+                a = line.split("=")[1]
+                a = float(a)
     
-        file1.write("num_flows {}     ".format(flows))
-        file1.write("Throughput: {}\n".format(a))
-        file1.write("-----------------------\n")
-        file1.flush()
-        flows += 20
+    file1.write("num_flows {}     ".format(flows))
+    file1.write("Throughput: {}\n".format(a/1000000000))
+    file1.write("-----------------------\n")
+    file1.flush()
+    flows += 1000
 
-    payload_size *= 2
 
 sock.close()
