db4d6f992b7b843c75a76ae4d19c24bf71fe92cd
ramnani <ramnani@localhost>
2023-04-09 01:29:36 -0600

    Working test script + some results

--- run_tests_client_rdp.py
+++ run_tests_client_rdp.py
@@ -3,7 +3,45 @@ import subprocess
 import time as tm
 import socket
 
-serv_ip            = "128.110.219.180"
+def get_throughput(a):
+    encoding          = 'utf-8'
+    output = a.decode(encoding)
+    for line in output.splitlines():
+        if "remote_throughput" in line:
+            a = [int(s) for s in line.split('=') if s.isdigit()][0]
+            print("a: ", a)
+            return a/1000000000
+
+
+def restart_iokernel():
+    timeout = 20
+    while 1:
+        subprocess.run("sudo pkill -9 iokerneld", shell=True)
+        subprocess.run("sudo pkill -9 iokerneld", shell=True)
+        subprocess.run("sudo pkill -9 iokerneld", shell=True)
+
+        subprocess.run("sudo pkill -9 tcp_stream", shell=True)
+        subprocess.run("sudo pkill -9 tcp_stream", shell=True)
+
+        tm.sleep(timeout)
+        timeout = timeout*2
+        subprocess.Popen("sudo ../iokerneld", shell=True)
+        tm.sleep(5)
+        start_signal = sock.recv(4)
+        tm.sleep(5)
+        a = subprocess.check_output("sudo ./tcp_stream -c -H 10.10.1.1 -F 1000 -T 25 --num-ports 25", shell=True)
+        tmp = get_throughput(a)
+        if tmp > 1:
+            data = "don0"
+            sock.send(data.encode())
+            return
+        data = "don1"
+        sock.send(data.encode())
+        
+
+
+
+serv_ip            = "128.110.219.182"
 control_plane_port = int(sys.argv[1])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,8 +51,10 @@ except:
     print("Connection to server failed")
     exit()
 
+# sock.recv(1024x)
 
-
+restart_iokernel()
+# quit()
 
 #/sudo ./tcp_stream -c -H 10.10.1.1 -F 10 -T 1
 #[config, nflows, nthreads, server_ip, time, payload, depth]
@@ -26,94 +66,111 @@ host_ip   = "10.10.1.1"
 f_command = "-F"
 t_command = "-T"
 numports_command = "--num-ports"
-nflows     = [1000, 5000, 10000, 15000, 20000, 30000, 40000, 50000, 75000, 100000, 200000, 500000, 750000, 1000000]
-# nthreads  = "1"
-nthreads  = [30, 50, 75]
-# nthreads  = [25, 50, 75]
-# nthreads = [50, 75]
-# kthreads  = [20, 30, 40]
-kthreads = [10, 20, 30, 40]
-# sthreads  = [25, 50, 75 , 100]
-sthreads = [5, 10, 25, 50]
-encoding = 'utf-8'
 
-a = '''# an example runtime config file
+nflows            = [1000, 5000, 20000, 50000, 75000, 100000]
+cthreads          = [5, 10, 25, 50]
+server_kthreads   = [5, 10, 20, 30, 40]
+client_kthreads   = [10, 20, 30, 40]
+sthreads          = [5, 10, 25, 50]
+encoding          = 'utf-8'
+
+config = '''# an example runtime config file
 host_addr 10.10.1.2
 host_netmask 255.255.255.0
 host_gateway 192.168.1.1
 runtime_kthreads {}
-#runtime_spinning_kthreads 2
-#runtime_guaranteed_kthreads 2
 runtime_priority lc
-#preferred_socket 0
-#disable_watchdog 1
-enable_directpath 1
+#enable_directpath 1
 #host_mtu 8000'''
 
-# fi = open("delete.config", "w")
-# fi.write(a)
-# exit()
+ckpt_flows = int(sys.argv[2])
+ckpt_s_kthreads = int(sys.argv[3])
+ckpt_c_kthreads = int(sys.argv[4])
+ckpt_sthreads = int(sys.argv[5])
+ckpt_cthreads = int(sys.argv[6])
 
-ckpt_flows = 500000
-ckpt_kthreads = 20
-ckpt_sthreads = 25
-ckpt_cthreads = 50
+
 
 for flows in nflows:
-    file1 = open("results/test_{}_rdp.txt".format(flows), "a")
     if flows < ckpt_flows:
         continue
-    for kt in kthreads:
-        if flows == ckpt_flows and kt < ckpt_kthreads:
+
+    file1 = open("new_results/test_{}_rdp.txt".format(flows), "a")
+    
+    for skt in server_kthreads:    
+        if flows == ckpt_flows and skt < ckpt_s_kthreads:
             continue
-        for threads in sthreads:
-            if flows == ckpt_flows and kt == ckpt_kthreads and threads < ckpt_sthreads:
+
+        fi = open("server.config", "w")
+        print(config.format(skt))
+        fi.write(config.format(skt))
+        fi.flush()
+        fi.close()
+
+        for ckt in client_kthreads:
+            if flows == ckpt_flows and skt == ckpt_s_kthreads and ckt < ckpt_c_kthreads:
                 continue
-            for client_threads in nthreads:
-                if threads > flows or client_threads > flows:
-                    break
-                if flows == ckpt_flows and  kt == ckpt_kthreads and threads == ckpt_sthreads and client_threads <= ckpt_cthreads:
+
+            for server_threads in sthreads:
+                if flows == ckpt_flows and skt == ckpt_s_kthreads and ckt == ckpt_c_kthreads and server_threads < ckpt_sthreads:
                     continue
-                while client_threads < flows/16384:
-                    client_threads = client_threads + 1
-                avg = []
-                i = 0
-                timeout = 5
-                while i < 3:
-                    print("Flows: {} Kthreads: {} Server_Threads: {} Client_Threads: {} Iteration round: {}\n".format(flows, kt, threads, client_threads, i))
-                    try:
-                        data = "start"
-                        sock.send(data.encode())
-                        # print("waiting to receive")
-                        # data_from_server = sock.recv(1024)
-                        # print("received")
-                        if client_threads >= 50:
-                            tm.sleep(timeout)
-                        tm.sleep(5)
-                        process = subprocess.check_output([su, command, client, host, host_ip, f_command, str(flows), t_command, str(threads), numports_command, str(client_threads)], timeout=1500)
-                        output = process.decode(encoding)
-                    except:
-                        timeout = timeout*2
-                        file1.write("SEG FAULT!!!!!!!!!!\n")
-                        output = ""
 
-                    data = "done"
-                    sock.send(data.encode())
-                    kill_signal = sock.recv(1024)
-
-                    if(kill_signal.decode() == "retry"):
+                for client_threads in cthreads:
+                    if flows == ckpt_flows and skt == ckpt_s_kthreads and ckt == ckpt_c_kthreads and server_threads == ckpt_sthreads and client_threads <= ckpt_cthreads:
                         continue
 
-                    for line in output.splitlines():
-                        if "remote_throughput" in line:
-                            a = [int(s) for s in line.split('=') if s.isdigit()][0]
-                            print("a: ", a)
-                            avg.append(a/1000000000)
-                            i += 1
+                    while client_threads < flows/16384:
+                        client_threads = client_threads + 1
+
+                    avg = []
+                    i = 0
+                    timeout = 5
+                    while i < 3:
+                        print("Flows: {} SKthreads: {} CKthreads: {} Server_Threads: {} Client_Threads: {} Iteration round: {}\n".format(flows, skt, ckt, server_threads, client_threads, i))
+                        try:
+                            data = "don0"
+                            sock.send(data.encode())
+
+                            #if client_threads >= 50:
+                            #    tm.sleep(
+                            tm.sleep(timeout)
+                            process = subprocess.check_output([su, command, client, host, host_ip, f_command, str(flows), t_command, str(server_threads), numports_command, str(client_threads)], timeout=400)
+                            output = process.decode(encoding)
+
+                            data = "don0"
+                        except:
+                            timeout = timeout*2
+                            print("ERROR!")
+                            file1.write("Error\n")
+                            output = ""
+                            data = "don1"
+                            #restart_iokernel()
+                        
+                        sent = sock.send(data.encode())
+                        print("sent: ", sent)
+                        kill_signal = sock.recv(4)
+
+                        if kill_signal.decode() == "don1" or data == "don1":
+                            restart_iokernel()
+                            continue
+
+                        for line in output.splitlines():
+                            if "remote_throughput" in line:
+                                a = [int(s) for s in line.split('=') if s.isdigit()][0]
+                                print("a: ", a)
+                                if a/1000000000 < 1:
+                                    thru = "don1"
+                                    sock.send(thru.encode())
+                                    restart_iokernel()
+                                    continue
+                                thru = "don0"
+                                sock.send(thru.encode())
+                                avg.append(a/1000000000)
+                                i += 1
 
                     
 
-                mean = sum(avg) / len(avg)
-                variance = sum([((x - mean) ** 2) for x in avg]) / len(avg)
-                file1.write("kthreads {} server_threads {} client_threads {} avg_throughput {} standard_deviation {} Max {} Min {}\n".format(kt, threads, client_threads, mean, variance ** 0.5, max(avg), min(avg)))
-                file1.flush()
+                    mean = sum(avg) / len(avg)
+                    variance = sum([((x - mean) ** 2) for x in avg]) / len(avg)
+                    file1.write("skthreads {} ckthreads {} server_threads {} client_threads {} avg_throughput {} standard_deviation {} Max {} Min {}\n".format(skt, ckt, server_threads, client_threads, mean, variance ** 0.5, max(avg), min(avg)))
+                    file1.flush()
