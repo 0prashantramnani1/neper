f4dda5c55b25a21149feaadb5cf155b6da306855
ramnani <ramnani@localhost>
2023-08-02 12:49:10 -0600

    test script changes

--- run_quantum_test.py
+++ run_quantum_test.py
@@ -64,12 +64,14 @@ host_ip   = "10.10.1.1"
 f_command = "-F"
 t_command = "-T"
 numports_command = "--num-ports"
-server_threads = 2
+server_threads = 1
 
-nflows            = {100:3, 500:3, 1000:3, 10000:3, 50000:3, 100000:3}
+# nflows            = {100:3, 500:3, 1000:3, 10000:3, 50000:3, 100000:3}
+nflows            = {600000:12}
 # nflows            = {10000:3, 20000:3, 30000:4, 40000:8}
 # time_quantum      = [15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100]
-time_quantum      = [20, 100]
+time_quantum      = [40000000000]
+receiver_kthreads = [20, 22, 24, 26, 28, 30, 32, 34, 38, 42, 44, 46]
 encoding          = 'utf-8'
 
 config = '''# an example runtime config file
@@ -83,71 +85,69 @@ runtime_priority lc
 #enable_directpath 1
 #host_mtu 8000
 runtime_quantum_us {}'''
+restart_iokernel()
 
 
-file1 = open("quantum_sheet_large_hash.txt", "a")
 
-for ts in time_quantum:
-    fi = open("sender.config", "w")
-    print(config.format(ts))
-    fi.write(config.format(ts))
-    fi.flush()
-    fi.close()
+file1 = open("600k_12ut_r.txt", "a")
 
-    for f, t in nflows.items():
-        if ts == 20:
-            if f < 50000:
-                continue
+for rkt in receiver_kthreads:
+    for ts in time_quantum:
+        fi = open("sender.config", "w")
+        print(config.format(ts))
+        fi.write(config.format(ts))
+        fi.flush()
+        fi.close()
 
-        timeout = 5
-        avg = []
-        i = 0
-        restart_iokernel()
-        print("Time_Quantum: {} Flows: {} Client_Threads: {}\n".format(ts, f, t))
-        while i < 1:
-            try:
-                data = "don0"
-                sock.send(data.encode())
+        for f, t in nflows.items():
+            timeout = 5
+            avg = []
+            i = 0
+            print("RKT: {} Flows: {} Client_Threads: {}\n".format(rkt, f, t))
+            while i < 1:
+                try:
+                    data = "don0"
+                    sock.send(data.encode())
 
-                tm.sleep(timeout)
-                process = subprocess.check_output([su, command, client, host, host_ip, f_command, str(f), t_command, str(server_threads), numports_command, str(t)])
-                output = process.decode(encoding)
+                    tm.sleep(timeout)
+                    process = subprocess.check_output([su, command, client, host, host_ip, f_command, str(f), t_command, str(server_threads), numports_command, str(t)])
+                    output = process.decode(encoding)
 
-                data = "don0"
-            except:
-                timeout = timeout + 5
-                print("ERROR!")
-                file1.write("Error\n")
-                output = ""
-                data = "don1"
-                #restart_iokernel()
+                    data = "don0"
+                except:
+                    timeout = timeout + 5
+                    print("ERROR!")
+                    file1.write("Error\n")
+                    output = ""
+                    data = "don1"
+                    #restart_iokernel()
 
-            sent = sock.send(data.encode())
-            print("sent: ", sent)
-            kill_signal = sock.recv(4)
+                sent = sock.send(data.encode())
+                print("sent: ", sent)
+                kill_signal = sock.recv(4)
 
-            if kill_signal.decode() == "don1" or data == "don1":
-                restart_iokernel()
-                continue
+                if kill_signal.decode() == "don1" or data == "don1":
+                    restart_iokernel()
+                    continue
 
-            for line in output.splitlines():
-                if "remote_throughput" in line:
-                    a = [int(s) for s in line.split('=') if s.isdigit()][0]
-                    print("a: ", a)
-                    if a/1000000000 < 1:
-                        thru = "don1"
+                for line in output.splitlines():
+                    if "remote_throughput" in line:
+                        a = [int(s) for s in line.split('=') if s.isdigit()][0]
+                        print("a: ", a)
+                        if a/1000000000 < 1:
+                            thru = "don1"
+                            sock.send(thru.encode())
+                            restart_iokernel()
+                            continue
+                        thru = "don0"
                         sock.send(thru.encode())
-                        restart_iokernel()
-                        continue
-                    thru = "don0"
-                    sock.send(thru.encode())
-                    avg.append(a/1000000000)
-                    i += 1
+                        avg.append(a/1000000000)
+                        i += 1
 
 
-        mean = sum(avg) / len(avg)
-        variance = sum([((x - mean) ** 2) for x in avg]) / len(avg)
-        file1.write("time_quantum {} Flows {} client_threads {} avg_throughput {} standard_deviation {} Max {} Min {}\n".format(ts, f, t, mean, variance ** 0.5, max(avg), min(avg)))
-        file1.flush()
+            mean = sum(avg) / len(avg)
+            variance = sum([((x - mean) ** 2) for x in avg]) / len(avg)
+            file1.write("rkt {} Flows {} client_threads {} avg_throughput {} standard_deviation {} Max {} Min {}\n".format(rkt, f, t, mean, variance ** 0.5, max(avg), min(avg)))
+            file1.flush()
         
 
